6158533389cbc361ed1297d037b2869ed55f661d
Progress2002 <ezeamakachisom2002@gmail.com>
2023-07-13 19:36:40 +0100

    Add model malidations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,6 @@
 class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :car
+
+  validates :city, :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :reservations, :cars, foreign_key: 'user_id'
+  has_many :cars, foreign_key: 'user_id'
+
+  validates :user_name, :email, :password, presence: true
 end
