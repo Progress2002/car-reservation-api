f0860213aa59bbc988bd3db6b98a51ffdd6463f1
Juwon <hardejuwon@gmail.com>
2023-07-25 19:20:31 +0100

    add user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(username: "sam", email: "sam@gmail.com", password: "password")
+  end
+  context "Testing validations" do
+    it "User should be invalid with username set to nil" do
+      subject.username = nil
+      expect(subject).to_not be_valid
+    end
+    it "User should be invalid with email set to nil" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+    it "User should be invalid with password set to nil" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+    it "User should be valid with presence of all valid attributes" do
+      expect(subject).to be_valid
+    end
+  end
 end
+
+
+
+
