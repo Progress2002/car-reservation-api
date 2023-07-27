b8cc4c1c8eaa99c1f96f5fb7156b3549942474bc
Progress2002 <ezeamakachisom2002@gmail.com>
2023-07-27 15:42:49 +0100

    Ypdate cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://car-reservation-app.onrender.com" # Update this with the actual URL of your frontend server
+    origins "http://localhost:3000" # Update this with the actual URL of your frontend server
 
     resource "*", 
       headers: :any,
