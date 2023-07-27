9a77331e2d53c2d60f2d624080ce99db5eea642b
Progress2002 <ezeamakachisom2002@gmail.com>
2023-07-27 12:10:50 +0100

    Fix cors issues with deplyment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*" # Update this with the actual URL of your frontend server
+    origins "https://car-reservation-app.onrender.com" # Update this with the actual URL of your frontend server
 
     resource "*", 
       headers: :any,
